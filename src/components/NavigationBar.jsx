a36621a3297cb307d3cb6a9b6578197a214f05d1
nryan1987 <nryan.brannan@gmail.com>
2021-07-13 20:59:54 -0500

    Update navigation bar to show collection statistics and styling. Add getCollectionStats, getTitlesAndPublishers to comicsService. Update getLatestIssues to take a parameter.

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,15 +1,47 @@
 import React, { Component } from "react";
 import { Navbar, Nav, Dropdown, Icon } from "rsuite";
 import store from "../store/Store";
+import {getCollectionStats} from "../services/comicsService.jsx"
 import "rsuite/dist/styles/rsuite-default.css";
+import "../css/NavigationBar.css"
 
 class NavigationBar extends Component {
 	constructor(props) {
 		super(props);
+
+		this.state = { 
+			averagePricePaid: 0.0,
+			averageValue: 0.0,
+			countOfComics: 0,
+			sumOfPricePaid: 0.0,
+			sumOfValue: 0.0
+		};
+	}
+
+	componentDidMount() {
+		var {jwt} = store.getState().user;
+		getCollectionStats(jwt).then(
+			(stats)=>{
+				this.setState({ averagePricePaid: stats.averagePricePaid });
+				this.setState({ averageValue: stats.averageValue });
+				this.setState({ countOfComics: stats.countOfComics });
+				this.setState({ sumOfPricePaid: stats.sumOfPricePaid });
+				this.setState({ sumOfValue: stats.sumOfValue });
+			}
+		);
+	}
+
+	goToPage(location) {
+		console.log("location", location);
+		this.props.history.push(location);
+	}
+
+	handleSettingsSelect = () => {
+		console.log("Settings");
 	}
 
 	render() {
-		var {username, jwt} = store.getState().user;
+		var {username} = store.getState().user;
 		return (
 			<Navbar>
 				<Navbar.Header>
@@ -19,8 +51,23 @@ class NavigationBar extends Component {
 				</Navbar.Header>
 				<Navbar.Body>
 					<Nav>
-						<Nav.Item icon={<Icon icon="home" />}>Home</Nav.Item>
-						<Nav.Item>News</Nav.Item>
+						{/* <Nav.Item onSelect={this.logOutput} icon={<Icon icon="home" />}>Home</Nav.Item> */}
+						{/* <NavLink href="/main">test</NavLink> */}
+						<button
+							onClick={() => {this.goToPage("/main")}}
+							type="submit"
+							className="btn navBarButton"
+						>
+							<Icon icon="home" />Home
+						</button>
+						<button
+							onClick={() => {this.goToPage("/newComics")}}
+							type="submit"
+							className="btn navBarButton"
+						>
+							<Icon icon="plus" />Add new comics
+						</button>
+						{/* <Nav.Item onSelect={this.logOutput}>News</Nav.Item>
 						<Nav.Item>Products</Nav.Item>
 						<Dropdown title="Token">
 							<Dropdown.Item>{jwt}</Dropdown.Item>
@@ -29,10 +76,15 @@ class NavigationBar extends Component {
 							<Dropdown.Item>Company</Dropdown.Item>
 							<Dropdown.Item>Team</Dropdown.Item>
 							<Dropdown.Item>Contact</Dropdown.Item>
-						</Dropdown>
+						</Dropdown> */}
 					</Nav>
 					<Nav pullRight>
-						<Nav.Item icon={<Icon icon="cog" />}>Settings</Nav.Item>
+						<Nav.Item>Total Comics: {this.state.countOfComics}</Nav.Item>
+						<Nav.Item>Total Price Paid: ${this.state.sumOfPricePaid}</Nav.Item>
+						<Nav.Item>Average Price Paid: ${this.state.averagePricePaid}</Nav.Item>
+						<Nav.Item>Total Value: ${this.state.sumOfValue}</Nav.Item>
+						<Nav.Item>Average Value: ${this.state.averageValue}</Nav.Item>
+						<Nav.Item icon={<Icon icon="cog" />} onSelect={this.handleSettingsSelect}>Settings</Nav.Item>
 					</Nav>
 				</Navbar.Body>
 			</Navbar>
--- src/services/comicsService.jsx
+++ src/services/comicsService.jsx
@@ -1,13 +1,13 @@
-export default async function getLast100Issues(jwt) {
-	console.log("getLast100Issues. token: " + jwt);
-
-	const response = await fetch("http://localhost:8080/comic/latest100", {
-		method: "GET",
+export default async function getLatestIssues(numIssues, jwt) {
+	console.log("getLast100Issues - numIssues: ", numIssues);
+	const response = await fetch("http://localhost:8080/comic/latestIssues", {
+		method: "POST",
 		headers: {
 			Accept: "application/json",
 			"Content-Type": "application/json",
 			"Authorization": "Bearer " + jwt
 		},
+		body: numIssues,
 	});
 	const responseJson = await response.json();
 	console.log("json: ", responseJson);
@@ -25,6 +25,59 @@ export async function getOneIssue(id, {jwt}) {
 			"Content-Type": "application/json",
 			"Authorization": "Bearer " + jwt
 		},
+	});
+	const responseJson = await response.json();
+	console.log("json: ", responseJson);
+
+	return responseJson;
+}
+
+export async function getCollectionStats(jwt) {
+	console.log("getCollectionStats. token: " + jwt);
+
+	const response = await fetch("http://localhost:8080/comic/collectionStats", {
+		method: "GET",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+			"Authorization": "Bearer " + jwt
+		},
+	});
+	const responseJson = await response.json();
+	console.log("json: ", responseJson);
+
+	return responseJson;
+}
+
+export async function getTitlesAndPublishers(jwt) {
+	console.log("getCollectionStats. token: " + jwt);
+
+	const response = await fetch("http://localhost:8080/comic/titles", {
+		method: "GET",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+			"Authorization": "Bearer " + jwt
+		},
+	});
+	const responseJson = await response.json();
+	console.log("json: ", responseJson);
+
+	return responseJson;
+}
+
+export async function addComicList(jwt, comicArray) {
+	console.log(comicArray);
+	console.log(JSON.stringify(comicArray));
+
+	const response = await fetch("http://localhost:8080/comic/addComics", {
+		method: "POST",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+			"Authorization": "Bearer " + jwt
+		},
+		body: JSON.stringify(comicArray),
 	});
 	const responseJson = await response.json();
 	console.log("json: ", responseJson);
