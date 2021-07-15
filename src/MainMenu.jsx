fbcbde1b1be69d9a1b2f4d2d1b031c98b062ee31
nryan1987 <nryan.brannan@gmail.com>
2021-07-14 23:19:06 -0500

    Update carousel in main menu.

--- src/MainMenu.jsx
+++ src/MainMenu.jsx
@@ -1,68 +1,59 @@
 import React, { Component } from "react";
 import NavigationBar from "./components/NavigationBar";
 import store from "./store/Store";
-import getLast100Issues from "./services/comicsService";
-import {Carousel} from '3d-react-carousal';
-import { isCompositeComponent } from "react-dom/cjs/react-dom-test-utils.production.min";
-
+import getLatestIssues from "./services/comicsService";
+import Carousel from 'react-bootstrap/Carousel';
+import "./css/MainMenu.css"
 
 class MainMenu extends Component {
+	pictureIntervalMS = 2000;
 	constructor(props) {
 		super(props);
 
 		console.log("MainMenu props: ", this.props);
 		console.log("Store:", store.getState());
 
-		this.state = { latest100: [], isLoading: true };
+		this.state = { carouselSlides: [], isLoading: true };
 	}
 
 	componentDidMount() {
-		console.log("MainMenu Component mounted");
-		var {jwt} = store.getState().user;
-		getLast100Issues(jwt).then(
-			(last100Issues)=>{
-				console.log(last100Issues);
-				var slides = [];
-				last100Issues.map((comic) => (slides.push(<img  src={"http://kandor/images/" + comic.picture}
-				alt={comic.title + " VOL: " + comic.volume + " #" + comic.issue } height='375px' width='250px'/>)));
-				console.log(slides);
+		this.getSlides();
+	}
 
-				this.setState({ latest100: slides });
+	getSlides = () => {
+		this.setState({isLoading: true});
+		var {jwt} = store.getState().user;
+		getLatestIssues(store.getState().user.userSettings.numRecentIssues, jwt).then(
+			(latestIssues)=>{
+				var slides = [];
+				latestIssues.map((comic) => (slides.push(<Carousel.Item><img key={Date.now()} src={"http://kandor/images/" + comic.picture}
+				alt={comic.title + " VOL: " + comic.volume + " #" + comic.issue} height='375px' width='250px'/>
+				<Carousel.Caption><p className="carouselCaption">{comic.title + " VOL: " + comic.volume + " #" + comic.issue}</p></Carousel.Caption>
+				</Carousel.Item>)));
+
+				this.setState({ carouselSlides: slides });
 				this.setState({isLoading: false});
 			}
 		);
-
-		try{
-		setInterval(async () => {
-			const last100Issues = await getLast100Issues(jwt);
-			var slides = [];
-
-			console.log(last100Issues);
-			last100Issues.map((comic) => (slides.push(<img key={Date.now()} src={"http://kandor/images/" + comic.picture}
-			alt={comic.title + " VOL: " + comic.volume + " #" + comic.issue } height='375px' width='250px'/>)));
-			console.log(slides);
-
-			this.setState({ latest100: slides });
-			this.setState({isLoading: false});
-		  }, 2000 * 100);
-		} catch(e) {
-		  console.log(e);
-		}
 	}
+
+	handleSelect = (selectedIndex, e) => {
+		//Reload the image list after it goes through the whole list.
+		if(selectedIndex === 0) {
+			this.getSlides();
+		}
+	  };
 
 	render() {
 		const isLoading = this.state.isLoading;
 		var main = <div>Loading...</div>;
 		if(!isLoading) {
-			main = <Carousel slides={this.state.latest100} autoplay={true} interval={2000}/>
-			//console.log(this.state.latest100);
+			main = <Carousel interval={this.pictureIntervalMS} onSelect={this.handleSelect}> {this.state.carouselSlides} </Carousel>
 		}
 		return (			
 			<div>
 				<div className="mainMenuDiv">
-					<NavigationBar/>
-					div1
-					{console.log(this.state.latest100)}
+					<NavigationBar history={this.props.history}/>
 					{main}
 				</div>
 				<div className="bottom-container" style={{float:"left"}}>div2</div>
