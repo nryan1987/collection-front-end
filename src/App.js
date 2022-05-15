e6541be985f2d2eafde091bcd2717a1b8db940c5
nryan1987 <nryan.brannan@gmail.com>
2022-05-15 12:59:36 -0500

    add meta tag to index.html

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-//import logo from "./logo.svg";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import store from "./store/Store";
-//import LoginForm from "./components/LoginForm";
-//import Navigation from "./components/Navigation";
 import Main from "./components/Main";
 
 function App() {
 	return (
 		<div className="App">
-			<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
-			{/* <LoginForm store={store} /> */}
-			{/* <Navigation /> */}
 			<Main />
 		</div>
 	);
